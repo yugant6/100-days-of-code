75fbb2ba53aee5c18154c03953555f543f8fa61d
yugant6 <34997974+yugant6@users.noreply.github.com>
2018-08-07 22:27:26 +0530

    Add files via upload

---
+++ day-1/forloop.js
@@ -0,0 +1,12 @@
+const days=['MON','TUE','WED','THU','FRI','SAT','SUN']
+
+for(let index=0; index<days.length; index++)
+{
+    console.log(days[index]);
+}
+console.log('\n');
+for(index=days.length-1;index>=0;index--)
+{
+    console.log(days[index]);
+    
+}
