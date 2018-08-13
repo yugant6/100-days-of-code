a0e64cc3e0943f229d0b0102b0052e9127cff4bd
yugant6 <34997974+yugant6@users.noreply.github.com>
2018-08-13 16:40:17 +0000

    coding on the go

---
+++ day-7/script.js
@@ -0,0 +1,29 @@
+
+//document.querySelector('button').addEventListener('click',()=>{
+// console.log("I am clicked");   
+//})
+//document.querySelector('button').addEventListener('click',(event)=>{
+  //  console.log(event);   
+  // })
+   //document.querySelector('button').addEventListener('click',(event)=>{
+  //  event.target.textContent = 'I was pressed'   
+//   })
+
+   // tracking input form
+
+ //  document.querySelector('#myform').addEventListener('change',(event)=>{
+//console.log(event.target.value);
+  // })
+
+  //document.querySelector('#myform').addEventListener('input',(event)=>{
+  //  console.log(event.target.value);
+  //  })
+  
+document.querySelector('.form').addEventListener('submit',(event)=>{
+  event.preventDefault()
+  console.log(event.target.username.value);
+console.log(event.target.realname.value);
+event.target.realname.value=''
+})
+
+   
