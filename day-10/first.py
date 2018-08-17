9b01a81a103d8205a125cdc81778bbc1d2c3e9d0
yugant6 <34997974+yugant6@users.noreply.github.com>
2018-08-17 23:53:09 +0530

    basics of python

---
+++ day-10/first.py
@@ -0,0 +1,18 @@
+print('hello World!')
+age = 14
+print(age)
+age = 15
+print(age)
+person1,person2,person3 = "shubham","yugant","rajat"
+print(person2)
+# remember that variables name is not having ny spaces
+# var 1 = "something" not works
+var1 =var2 =var3 = "apples"
+print(var2)
+age1 =20
+age2 = 59
+age3 =age1 + age2
+print(age3)
+age3= age2 - age1
+print(age3)
+print(age2/age1)
