063bf461467922ad225a6829a24d21ed81b5948b
yugant6 <34997974+yugant6@users.noreply.github.com>
2018-08-18 23:51:07 +0530

    more basic stuff in python

---
+++ day-11/conditions.py
@@ -0,0 +1,28 @@
+if 5>2:
+  print("true") 
+  print("""This is a
+multiline docstring.""")
+"""This is a
+multiline docstring or comments."""
+x = "awesome"
+print("Python is " + x)
+x = "Python is "
+y = "awesome"
+z =  x + y
+print(z)
+"""x = 5
+y = "John"
+print(x + y)"""
+# integer and string not add
+print(str(5)+"is the johns fav number")
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+a = "yug" #string
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+z = int("3") # it converts all the values into integer
+x = float(1) # it converts all the values into float
+y = str(2) # it converts all the values into string
