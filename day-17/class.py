53e8b576803abef68fbb47ca1a0b5c7e7e3464c5
yugant6 <34997974+yugant6@users.noreply.github.com>
2018-08-25 23:47:28 +0530

    create a class in python

---
+++ day-17/class.py
@@ -0,0 +1,15 @@
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age) 
