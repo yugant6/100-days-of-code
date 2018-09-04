2fcb796e0c8b735bc0c414c409609e08caf0cbd4
yugant6 <34997974+yugant6@users.noreply.github.com>
2018-09-04 23:49:39 +0530

    index file in react360

---
+++ day-25/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  VrButton,
+  Environment,
+  NativeModules,
+} from 'react-360';
+
+const {VideoModule} = NativeModules;
+// Create a player
+VideoModule.createPlayer('myplayer');
+// Play a specific video
+VideoModule.play('myplayer', {
+  source: {url: './static_assets/vdo.mp4'}, // provide the path to the video
+  stereo: '2D'});
+export default class Hello360 extends React.Component {
+
+  render() {
+  return (
+    <View style={styles.panel}>
+      <VrButton
+        onClick={()=>{
+Environment.setBackgroundVideo('myplayer');
+}}
+        style={styles.greetingBox}>
+        <Text style={styles.greeting}>
+          {`Press to play video in background`}
+        </Text>
+      </VrButton>
+    </View>
+  );
+}
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('Hello360', () => Hello360);
