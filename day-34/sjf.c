23ebddd4739517468954f91096cf8e347f7ffc15
yugant6 <34997974+yugant6@users.noreply.github.com>
2018-09-22 23:31:09 +0530

    sjf non primitive in c

--- day-34/sjf.c
+++ day-34/sjf.c
@@ -1,80 +1,63 @@
 #include<stdio.h>
-
+ 
 void main()
 {
- int e,f,g,h,j,k,temp2,l,m,temp,temp1,c,c2,c1,n,i,bt[10],at[10],wt[10],avgwt,tat[10],avgtat;
- printf("Enter the number of processes");
- scanf("%d",&n);
- printf("Enter the burst time of process");
- for(i=0;i<n;i++)
-{
- printf("Enter burst time of p%d process",i+1);
- scanf("%d",&bt[i]);
-}
-
- printf("Enter the arrival time of processes ");
-for(i=0;i<n;i++)
-{
- printf("Enter the arrival time of p%d process",i+1);
- scanf("%d",&at[i]);
-}
-for(i=0;i<n;i++)
-{
-if(bt[i]>bt[i+1])
-{
- bt[i+1]=bt[i];
-}}
-for(i=0;i<n;i++){
-if(at[i]==0){
-tat[i]=bt[i];
-e=tat[i];
-i=c;
-temp= bt[i];
-wt[i] = tat[i]-bt[i];
-f=wt[i];
-}
-}
-
-for(i=0;i<n;i++)
-{
- if(at[i]==c)
- continue;
- tat[i] = (temp+bt[i])- at[i]; 
- g=tat[i];
-  wt[i] = tat[i] -bt[i];
- h=wt[i];
-temp1 =bt[i];
- printf("%d",i);
- i=c1;
-}
-
-for(i=0;i<n;i++)
-{
- if(i==c | i==c1)
- continue;
- tat[i] = (temp+temp1+bt[i])-at[i];
- j=tat[i];
- wt[i] = tat[i]- bt[i];
- k=wt[i]; 
-temp2= bt[i];
- printf("%d",i);
- i =c2;
-}
-
-for(i=0;i<n;i++)
-{
- if(i==c | i==c1 |i==c2)
- continue;
- tat[i] = (temp+temp1+temp2+bt[i])-at[i];
- l=tat[i];
-wt[i]= tat[i]-bt[i];
-  m=wt[i];
-printf("%d",i);
-}
-
-avgtat=(e+g+j+l)/4;
-avgwt= (k+m+h+f)/4;
-printf("avg tt %d",avgtat);
-printf("avg wt %d",avgwt);
-
+    int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
+    float avg_wt,avg_tat;
+    printf("Enter number of process:");
+    scanf("%d",&n);
+ 
+    printf("\nEnter Burst Time:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("p%d:",i+1);
+        scanf("%d",&bt[i]);
+        p[i]=i+1;           //contains process number
+    }
+ 
+    //sorting burst time in ascending order using selection sort
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[j]<bt[pos])
+                pos=j;
+        }
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+ 
+    wt[0]=0;            //waiting time for first process will be zero
+ 
+    //calculate waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+ 
+        total+=wt[i];
+    }
+ 
+    avg_wt=(float)total/n;      //average waiting time
+    total=0;
+ 
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];     //calculate turnaround time
+        total+=tat[i];
+        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+    }
+ 
+    avg_tat=(float)total/n;     //average turnaround time
+    printf("\n\nAverage Waiting Time=%f",avg_wt);
+    printf("\nAverage Turnaround Time=%f\n",avg_tat);
 }
